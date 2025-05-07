8cc284d5711c03705ba12a859d2800e7f0cbe96c
AlexMartin <alex.martin.7e7@itb.cat>
2025-05-07 12:47:31 +0200

    commit exercicis practicar

--- Apuntes/practicarFuncions.sql
+++ Apuntes/practicarFuncions.sql
@@ -102,15 +102,44 @@ city_new VARCHAR(30) not null,
 changed_on TIMESTAMP(6) NOT NULL);
 
 CREATE OR REPLACE FUNCTION func_registrar_canvis_loc() 
-
+RETURNS trigger AS $$
+BEGIN
+    IF NEW.city != OLD.city THEN
+        INSERT INTO canvis_locations(location_id,city_old,city_new,changed_on)
+        VALUES (OLD.location_id,OLD.city,NEW.city,NOW());
+    ELSE
+       RAISE EXCEPTION 'NO es pot modificar locations , el nou city % a de ser diferent al old % %',NEW.city,OLD.city; 
+    END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trig_registrar_canvis_loc
-BEFORE INSERT OR UPDATE ON employees
+AFTER UPDATE ON locations
 FOR EACH ROW
 EXECUTE FUNCTION func_registrar_canvis_loc();
 
+CREATE TABLE canvis (
+id serial,
+timestamp_ TIMESTAMP WITH TIME ZONE default NOW(),
+nom_trigger text,
+tipus_trigger text,
+nivell_trigger text,
+ordre text );
 
+CREATE OR REPLACE FUNCTION func_registrar_canvis()
+RETURNS trigger AS $$
+BEGIN
+        INSERT INTO canvis(timestamp_,nom_trigger,tipus_trigger,nivell_trigger,ordre)
+        VALUES (NOW(),TG_NAME, TG_WHEN,TG_LEVEL,TG_OP);
+RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
 
+CREATE TRIGGER trig_gravar_operacions
+AFTER UPDATE ON locations
+FOR EACH STATEMENT
+EXECUTE FUNCTION func_registrar_canvis();
 
 /*
 Exercici 3
@@ -130,5 +159,74 @@ utilitzant una excepció. Aquesta funció la cridaras en el procediment proc_mos
 una excepció amb el missatge "El departament no existeix" en cas que retorni FALSE i que cridarà la funció
 func_emp_mes_antic si retorna TRUE. Realitza el joc de proves corresponent.
 */
+
+CREATE TYPE dades_emp_type AS (
+    var_empID numeric (11),
+    var_nom varchar(20),
+    var_cognom varchar(25));
+
+CREATE OR REPLACE FUNCTION func_comprv_dept(param_dept_name departments.department_name%TYPE)
+RETURNS BOOLEAN LANGUAGE plpgsql AS $$
+DECLARE
+    var_deptId departments.department_id%TYPE;
+BEGIN
+    SELECT department_id 
+    INTO STRICT var_deptId
+    FROM departments
+    WHERE department_name = param_dept_name;
+    RETURN TRUE;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN FALSE;
+    WHEN OTHERS THEN
+        RAISE EXCEPTION 'Error inesperat comprovant el departament: %', SQLERRM;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION func_emp_mes_antic() RETURNS dades_emp_type language plpgsql AS $$
+DECLARE
+    var_dades_emp dades_emp_type;
+BEGIN
+    SELECT employee_id,first_name,last_name
+    INTO var_dades_emp
+    FROM employees e JOIN departaments d USING(department_id)
+    WHERE d.department_name = param_dept_name AND e.hire_date ( SELECT MIN(hire_date) 
+                                                                FROM employees e JOIN departaments d
+                                                                USING(department_id)
+                                                                WHERE department_name = param_dept_name);
+    RETURN var_dades_emp
+END;
+
+CREATE OR REPLACE PROCEDURE proc_mostrar_emp_dept(param_dept_name DEPARTMENTS.department_name%TYPE)  
+LANGUAGE plpgsql AS $$
+DECLARE
+    var_dades_emp dades_emp_type;
+BEGIN
+    IF func_comprv_dept(param_dept_name) IS TRUE THEN
+        var_dades_emp:=(SELECT func_emp_mes_antic(param_dept_name);)
+    RAISE NOTICE 'L`empleat més antic del departament % te : ID % NOM % COGNOM '
+    param_dept_name,
+    var_dades_emp.var_empID,
+    var_dades_emp.var_nom,
+    var_dades_emp.var_cognom;
+    ELSE 
+      RAISE EXCEPTION 'El departament no existeix'
+      USING hint ='Introdueix altre departament';
+    END IF;
+END;$$;
+
+--Joc de proves
+--Cridem el procediment i li passem un paràmetre amb un nom de departament que existeix
+CALL proc_mostrar_emp_dept('IT');
+
+--Mostra
+L`empleat més antic del department IT té l`ID 103 i es diu Alexander Hunold
+
+--Cridem el procediment i li passem un paràmetre amb un nom de departament que no existeix
+CALL proc_mostrar_emp_dept(';ar');
+
+--Salta l'excepció
+[P0001] ERROR: El departament no existeix Hint: Introdueix altre departament! 
+Where: PL/pgSQL function proc_mostrar_emp_dept(character varying) line 9 at RAISE
 
 
